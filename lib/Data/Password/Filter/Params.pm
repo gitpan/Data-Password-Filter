8458301a55295b74d24cf7d9f83a6b266a942674
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2014-10-26 18:28:06 +0000

    Import of MANWAR/Data-Password-Filter-0.09 from CPAN.
    gitpan-cpan-distribution: Data-Password-Filter
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         MANWAR/Data-Password-Filter-0.09.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/Data/Password/Filter/Params.pm
+++ lib/Data/Password/Filter/Params.pm
@@ -1,6 +1,6 @@
 package Data::Password::Filter::Params;
 
-$Data::Password::Filter::VERSION = '0.08';
+$Data::Password::Filter::VERSION = '0.09';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Data::Password::Filter::Params - Placeholder for parameters for Data::Password::
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
